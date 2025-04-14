7564b4d6c3db2177529536042bfdd2e2e4752bcc
DianaZelenska <diastrekoza@gmail.com>
2025-04-14 11:19:34 +0200

    Add files via upload

---
+++ Auto/Auto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoneu
+{
+    public class Auto
+    {
+        private string farbe;
+        private int baujahr;
+        private string marke;
+        private int hoechstgeschwindigkeit;
+        private int aktGeschw;
+        //private bool gestartet;
+
+
+        // Variablen markieren, rechte Taste -> Schnellaktionen -> Feld kapseln(weiterhin verwenden)
+        public string Farbe { get => farbe; set => farbe = value; }
+        public int Baujahr { get => baujahr; set => baujahr = value; }
+        public string Marke { get => marke; set => marke = value; }
+
+        public Auto(string Farbe, int bj, string Marke, int hg)
+        {
+            farbe = Farbe;
+            baujahr = bj;
+            marke = Marke;
+            hoechstgeschwindigkeit = hg;
+            aktGeschw = 0;
+            //gestartet = false;
+        }
+        public void tacho()
+        {
+            Console.WriteLine("Der " + farbe + "e " + marke + " fährt " + aktGeschw.ToString() + "kmh");
+        }
+
+        public void bremsen(int um)
+        {
+            aktGeschw -= um;
+
+            if (aktGeschw < 0)
+                aktGeschw = 0;
+            tacho();
+        }
+
+        public void beschleunigen(int um)
+        {
+            aktGeschw += um;
+            tacho();
+        }
+    }
+}
---
+++ Auto/Program.cs
@@ -0,0 +1,115 @@
+﻿using Autoneu;
+
+public class Program
+{
+    private static Auto[] autos;
+    private static void Main(string[] args)
+    {
+        autos = new Auto[2];
+        //autos[0] = new Auto("grau", 1988, "Trabbi", 180);
+        //autos[1] = new Auto("gelb", 1966, "Wartburg", 130);
+        //autos[2] = new Auto("blau", 1979, "Lada", 110);
+
+        autoerstellen();
+       
+        int autonr = 0;
+
+        while((autonr = autowahl()) != -1)
+        {
+            autobewegen(autos[autonr]);
+        }
+
+        //Auto auto = new Auto("grau", 1988, "Trabbi", 180);
+    }
+
+    private static void autoerstellen()
+    {
+        string farbe;
+        string marke;
+        int baujahr;
+        int hg;
+
+        for (int i = 0; i < autos.Length; i++)
+        {
+            Console.WriteLine("Farbe: ");
+            farbe = Console.ReadLine();
+            Console.WriteLine("Marke: ");
+            marke = Console.ReadLine();
+            Console.WriteLine("Baujahr: ");
+            int.TryParse(Console.ReadLine(), out baujahr);
+            Console.WriteLine("Höchstgeschwindigkeit: ");
+            int.TryParse(Console.ReadLine(), out hg);
+
+            autos[i] = new Auto(farbe, baujahr, marke, hg);
+        }
+        
+    }
+
+    // das war vorher in main
+    private static void autobewegen(Auto auto)
+    {
+        string s = "1";
+
+        auto.tacho();
+
+        for (; s != "0";)
+        {
+            s = menue();
+
+            switch (s)
+            {
+                case "1":
+                    auto.beschleunigen(wieviel());
+                    break;
+                case "2":
+                    auto.bremsen(wieviel());
+                    break;
+                
+            }
+        }
+    }
+
+    private static int autowahl()
+    {
+        int which = 0;
+        string s;
+
+        for(int i = 0; i < autos.Length; i++)
+        {
+            Console.WriteLine((i+1).ToString() + " " + autos[i].Marke);
+        }
+
+        Console.WriteLine("0 Ende");
+
+        s = Console.ReadLine();
+        int.TryParse(s, out which);
+
+        return which - 1;
+    }
+
+    // einlesen, um wie viel beschleubigen oder bremsen
+    private static int wieviel()
+    {
+        string s = "";
+        int x = 0;
+
+        Console.Write("Um wieviel: ");
+        s = Console.ReadLine();
+
+        int.TryParse(s, out x);
+
+        return x;
+    }
+
+    private static string menue()
+    {
+
+        Console.WriteLine("1 Beschleunigen");
+        Console.WriteLine("2 Bremsen");
+        Console.WriteLine("0 Ende");
+
+        string s = Console.ReadLine();
+
+        return s;
+    }
+}
